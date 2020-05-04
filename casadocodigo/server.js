7b143370cbe12c79f49f81d5c20684d531a485a6
Brendon Alex <brendon_gama@hotmail.com>
2020-05-04 16:27:56 -0300

    Encapsulando as rotas

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -1,31 +1,5 @@
-const http = require('http');
+const app = require('./src/config/custom-expres');
 
-const servidor  = http.createServer(function (req, resp) {
-
-    let html = '';
-    if (req.url == '/') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-    `;
-    } else if (req.url == '/livros')
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> teste Listagem de livros </h1>
-            </body> 
-        </html>
-    `;
-
-    resp.end(html);
-});
-servidor.listen(3000);
+app.listen(3000, function() {
+    console.log('Servidor rodando na porta 3000');
+})
